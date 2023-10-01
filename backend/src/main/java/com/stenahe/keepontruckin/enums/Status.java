deb9bc4aaf9f4fe84a290dd970e1db5fec16c655
Santiago Tena Hernandez <stenahe@gmail.com>
2023-10-01 21:22:06 +0200

    Simplify enums

--- backend/src/main/java/com/stenahe/keepontruckin/enums/Status.java
+++ backend/src/main/java/com/stenahe/keepontruckin/enums/Status.java
@@ -8,48 +8,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Status {
     public enum DriverStatus {
-        REST("rest"),
-        DRIVING("driving");
-
-        private final String value;
-
-        DriverStatus(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
+        REST,
+        DRIVING
     }
 
     public enum TruckStatus {
-        OK("ok"),
-        NOK("nok");
-
-        private final String value;
-
-        TruckStatus(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
+        OK,
+        NOK
     }
 
     public enum CargoStatus {
-        READY("ready"),
-        SHIPPED("shipped"),
-        DELIVERED("delivered");
-
-        private final String value;
-
-        CargoStatus(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
+        READY,
+        SHIPPED,
+        DELIVERED
     }
 }
